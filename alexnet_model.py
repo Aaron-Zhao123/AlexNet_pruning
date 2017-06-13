268ed43928523fd1af40487754b247961c38e804
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-06-13 11:50:27 +0100

    auto git add all

--- alexnet_model.py
+++ alexnet_model.py
@@ -24,7 +24,7 @@ class alexnet(object):
         imgs = self.images
 
         conv1 = self.conv_layer(imgs, 'conv1', padding = 'VALID', stride = 4, prune = True)
-        pool1 = self.maxpool(lrn1, 'pool1', 3, 2, padding = 'VALID')
+        pool1 = self.maxpool(conv1, 'pool1', 3, 2, padding = 'VALID')
         lrn1 = self.lrn(pool1, 'lrn1')
 
         conv2 = self.conv_layer(lrn1, 'conv2', prune = True, split = 2)
