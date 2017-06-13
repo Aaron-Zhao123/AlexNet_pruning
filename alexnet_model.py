75ad0d35dbfb195aa0252ad91c1572c0f6907b2c
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-06-13 10:11:29 +0100

    auto git add all

--- alexnet_model.py
+++ alexnet_model.py
@@ -128,7 +128,7 @@ class alexnet(object):
         self.test = 'hi'
         self.images = tf.placeholder(tf.float32, [None, 224, 224, 3], 'input')
         self.labels = tf.placeholder(tf.int32, [None], 'label')
-        self.train_phase = tf.placeholder(tf.bool, name = 'isTrain')
+        self.isTrain = tf.placeholder(tf.bool, name = 'isTrain')
 
     def _get_variables(self, isload, weights_path = 'DEFAULT'):
         """
